b50008ae16a52ec96c80324a6170c720c94f8814
Miro Mäkinen <57602064+Miroskaja@users.noreply.github.com>
2023-11-07 13:07:29 +0200

    Update create_table.sql

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE ruuvitietokanta;
 USE ruuvitietokanta;
-CREATE TABLE ruuvitietokanta (id int NOT NULL AUTO_INCREMENT, temperature NUMERIC, pressure NUMERIC, humidity NUMERIC, Battery_Voltage NUMERIC, TX_Power NUMERIC);
+CREATE TABLE ruuvitietokanta (id int NOT NULL AUTO_INCREMENT, temperature NUMERIC, pressure NUMERIC, humidity NUMERIC, Battery_Voltage NUMERIC, TX_Power NUMERIC, PRIMARY KEY (id);
